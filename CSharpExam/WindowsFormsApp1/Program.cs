0d5e2cce07af6355c8e21237a23a24161eb1f541
Daiqian Hu <daih@microsoft.com>
2017-11-29 13:51:56 +0800

    Add delegate demo code.

--- CSharpExam/WindowsFormsApp1/Program.cs
+++ CSharpExam/WindowsFormsApp1/Program.cs
@@ -17,6 +17,29 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            MethodInvoker x = CreateDelegateInstance();
+
+            x();
+
+            x();
+        }
+
+        static MethodInvoker CreateDelegateInstance()
+        {
+            int counter = 5;
+
+            MethodInvoker ret = delegate
+            {
+                MessageBox.Show(counter.ToString());
+                Console.WriteLine(counter);
+
+                counter++;
+            };
+
+            ret();
+
+            return ret;
         }
     }
 }
