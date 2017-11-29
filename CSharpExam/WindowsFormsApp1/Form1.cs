f47e02b9624818c83eb789bee014eede587a0d56
Daiqian Hu <daih@microsoft.com>
2017-11-29 14:27:36 +0800

    Add local variables are instanced sample code.
    Add UI to show sample outputs.

--- CSharpExam/WindowsFormsApp1/Form1.cs
+++ CSharpExam/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,65 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        public MethodInvoker CreateDelegateInstance()
+        {
+            int counter = 5;
+
+            MethodInvoker ret = delegate
+            {
+                richTextBox1.AppendText(counter.ToString() + Environment.NewLine); 
+                Console.WriteLine(counter);
+
+                counter++;
+            };
+
+            ret();
+
+            return ret;
+        }
+
+        //
+        // Demo how local variables are instanced
+        //
+        public void DemoLocalVarInst()
+        {
+            richTextBox1.AppendText("== local variables are instanced==" + Environment.NewLine);
+
+            List<MethodInvoker> list = new List<MethodInvoker>();
+
+            for(int index = 0; index < 5; index++)
+            {
+                int counter = index * 10;
+                list.Add(delegate
+                {
+                    richTextBox1.AppendText(counter.ToString() + Environment.NewLine);
+                    Console.WriteLine(counter);
+                    counter++;
+                });
+            }
+
+            foreach(MethodInvoker t in list)
+            {
+                t();
+            }
+
+            list[0]();
+            list[0]();
+            list[0]();
+
+            list[1]();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MethodInvoker x = CreateDelegateInstance();
+
+            x();
+
+            x();
+
+            DemoLocalVarInst();
+        }
     }
 }
